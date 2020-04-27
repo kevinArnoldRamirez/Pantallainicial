933b49a8b097e561ab52163910ac82dd3408c480
kevinArnoldRamirez <64047619+kevinArnoldRamirez@users.noreply.github.com>
2020-04-27 12:28:04 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/isc/tesoem/arnold0824/pantallainicial/PrincipalActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.isc.tesoem.arnold0824.pantallainicial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
